3bd4c53466305a1bc35235d9913fcf0532ad4845
Solomon <solomonuche@gmail.com>
2023-12-22 21:17:57 +0100

    puppet install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,25 @@
 # Install a python flask package with puppet
 
+# Install Python 3.8.10
+package { 'python3.8':
+  ensure => '3.8.10',
+}
+
+# Install pip
+package { 'python3-pip':
+  ensure => present,
+}
+
+# Install Flask 2.1.0
 package { 'flask':
   ensure   => '2.1.0',
-  name     => 'flask',
-  provider => 'pip3',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
+# Install Werkzeug 2.1.1
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
