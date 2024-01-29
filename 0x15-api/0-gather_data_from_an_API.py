ac13fd7e9f8367e02113c7d7a92a4f2ea852d8d5
Solomon <solomonuche@gmail.com>
2024-01-29 22:39:34 +0100

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,24 +4,5 @@ import requests
 from sys import argv
 
 
-employee_url = 'https://jsonplaceholder.typicode.com/users'
-employee_todos = 'https://jsonplaceholder.typicode.com/todos'
-response = requests.get(employee_url, params={"id": employee_id})
-employee_res = response.json()
-employee_name = employee_res[0].get('name')
-response = requests.get(employee_todos, params={"userId": employee_id})
-employee_todos = response.json()
-#TOTAL_NUMBER_OF_TASKS = len(employee_todos)
-#NUMBER_OF_DONE_TASKS = 0
-#COMPLETED_TASKS = []
-
-for i in employee_todos:
-    if i.get("completed"):
-        #NUMBER_OF_DONE_TASKS += 1
-        #COMPLETED_TASKS.append(i)
-
-print(f"Employee {employee_name} is done with tasks({NUMBER_OF_DONE_TASKS}"
-      f"/{TOTAL_NUMBER_OF_TASKS}):")
-
-#for i in COMPLETED_TASKS:
-    #print(f"\t {i.get('title')}")
+if __name__ == "__main__":
+    pass
