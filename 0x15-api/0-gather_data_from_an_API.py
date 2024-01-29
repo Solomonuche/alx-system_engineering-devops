30054bb162506e6ca50d7f557de578082da68c8e
Solomon <solomonuche@gmail.com>
2024-01-29 21:49:59 +0100

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-a Python script that, using this REST API, for a given employee ID
+""" a Python script that, using this REST API, for a given employee ID
 returns information about his/her TODO list progress.
 """
 
