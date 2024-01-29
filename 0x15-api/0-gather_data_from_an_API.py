263778e55301958b5b0d344f7f2eef13c2833d6b
Solomon <solomonuche@gmail.com>
2024-01-29 22:36:53 +0100

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,17 +13,17 @@ employee_res = response.json()
 employee_name = employee_res[0].get('name')
 response = requests.get(employee_todos, params={"userId": employee_id})
 employee_todos = response.json()
-TOTAL_NUMBER_OF_TASKS = len(employee_todos)
-NUMBER_OF_DONE_TASKS = 0
-COMPLETED_TASKS = []
+#TOTAL_NUMBER_OF_TASKS = len(employee_todos)
+#NUMBER_OF_DONE_TASKS = 0
+#COMPLETED_TASKS = []
 
 for i in employee_todos:
     if i.get("completed"):
-        NUMBER_OF_DONE_TASKS += 1
-        COMPLETED_TASKS.append(i)
+        #NUMBER_OF_DONE_TASKS += 1
+        #COMPLETED_TASKS.append(i)
 
 print(f"Employee {employee_name} is done with tasks({NUMBER_OF_DONE_TASKS}"
       f"/{TOTAL_NUMBER_OF_TASKS}):")
 
-for i in COMPLETED_TASKS:
-    print(f"\t {i.get('title')}")
+#for i in COMPLETED_TASKS:
+    #print(f"\t {i.get('title')}")
