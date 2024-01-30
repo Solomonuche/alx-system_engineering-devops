be460a8aeb0515bf127dc623dc72dcb756ba9642
Solomon <solomonuche@gmail.com>
2024-01-30 02:12:44 +0100

    Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -14,30 +14,17 @@ if __name__ == "__main__":
     for employee_id in employee_ids:
         response = requests.get(employee_url, params={"id": employee_id})
         employee_res = response.json()
-        employee_name = employee_res[0].get('name')
         username = employee_res[0].get('username')
 
         response = requests.get(todos_url, params={"userId": employee_id})
         employee_todos = response.json()
-        TOTAL_NUMBER_OF_TASKS = len(employee_todos)
-        NUMBER_OF_DONE_TASKS = 0
-        COMPLETED_TASKS = []
         obj_list = []
 
         for i in employee_todos:
-            if i.get("completed"):
-                NUMBER_OF_DONE_TASKS += 1
-                COMPLETED_TASKS.append(i)
             title = f'{i.get("title")}'
             status = i.get("completed")
             obj = {"username": username, "task": title, "completed": status}
             obj_list.append(obj)
-
-        print(f"Employee {employee_name} is done with "
-              f"tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
-
-        for i in COMPLETED_TASKS:
-            print(f"\t {i.get('title')}")
 
         obj_dict[employee_id] = obj_list
 
