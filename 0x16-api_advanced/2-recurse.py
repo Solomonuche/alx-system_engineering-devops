a3ddb4f0926e592a56accd95af5befe23f5acd86
Solomon <solomonuche@gmail.com>
2024-02-14 01:49:43 +0100

    Recurse it!

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""a recursive function that queries the Reddit API and returns
+a list containing the titles of all hot articles for a given subreddit"""
+
+import requests
+
+
+def recurse(subreddit, after=None, hot_list=[]):
+    """a recursive function that queries the Reddit API and returns a list
+    containing the titles of all hot articles for a given subreddit"""
+
+    if subreddit:
+        params = {}
+        url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+        headers = {'User-Agent': 'Mozilla/5.0'}
+
+        if after:
+            params['after'] = after
+        res = requests.get(url, params=params, headers=headers).json()
+
+        if 'data' in res and 'children' in res['data']:
+            if len(res['data']['children']) > 0:
+                for post in res['data']['children']:
+                    hot_list.append(post['data']['title'])
+
+                after = res['data']['after']
+
+                if after is None:
+                    return hot_list
+
+                recurse(subreddit, after, hot_list)
+
+                return hot_list
+            else:
+                return None
+    else:
+        return None
