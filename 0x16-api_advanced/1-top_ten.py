0a994a9912e624e9d917b4613fb782fbdf4fe78b
Solomon <solomonuche@gmail.com>
2024-02-15 19:10:15 +0100

    test fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,17 +9,14 @@ def top_ten(subreddit):
     """a function that queries the Reddit API and prints the titles
     of the first 10 hot posts listed for a given subreddit."""
 
-    if subreddit:
-        url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-        res = requests.get(url,
-                           headers={'User-Agent': 'My-User-Agent'},
-                           allow_redirects=False)
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    res = requests.get(url,
+                       headers={'User-Agent': 'My-User-Agent'},
+                       allow_redirects=False)
 
-        if sub_info.status_code >= 300:
-            print('None')
-        else:
-            res = res.json()
-            for post in res['data']['children']:
-                print(post['data']['title'])
-    else:
+    if res.status_code >= 300:
         print('None')
+    else:
+        res = res.json()
+        for post in res['data']['children']:
+            print(post['data']['title'])
