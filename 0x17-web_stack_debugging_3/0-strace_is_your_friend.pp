f48c42fcd16503006a550d5f9cbc7d71aae9334e
Solomon <solomonuche@gmail.com>
2024-02-15 01:49:29 +0100

    puppet

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# fix Apache (500) error by fixing typo in wordpress
+exec { 'typo fix':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
