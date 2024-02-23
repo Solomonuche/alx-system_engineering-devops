8be8a72360fbc93e46ea47b48bbf4a59fa2bfb42
Solomon <solomonuche@gmail.com>
2024-02-23 01:40:40 +0100

    let's bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,12 @@
 # Increases the amount of traffic an Nginx server can handle
 
-exec {'sets file limite for nginx':
-  command => 'sed -i "s/15/2000/g" /etc/default/nginx',
-  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
-  onlyif  => 'test -f /etc/default/nginx'
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Restart Nginx
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
