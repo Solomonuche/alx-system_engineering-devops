163cfe19f03c9bc65d8b1e45cea45059d02469b6
Solomon <solomonuche@gmail.com>
2024-02-23 01:34:30 +0100

    increase the limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,15 +1,7 @@
 # Increases the amount of traffic an Nginx server can handle
 
-exec { 'fix-for-nginx':
-  command     => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path        => '/usr/local/bin:/bin',
-  refreshonly => true,
+exec {'sets file limite for nginx':
+  command => 'sed -i "s/15/2000/g" /etc/default/nginx',
+  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
+  onlyif  => 'test -f /etc/default/nginx'
 }
-
-# Restart Nginx when the configuration file is modified
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
-  require => Exec['fix-for-nginx'],
-}
-
